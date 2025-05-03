c9939a011b9ef0f11ba853e8e53f7de37d2bc4ee
Steve Newstead <steve@macbookair.lan>
2025-05-03 17:47:22 +0100

    check more exact match

--- src/utils/spotify.ts
+++ src/utils/spotify.ts
@@ -91,7 +91,7 @@ class SpotifyAPI {
     });
 
     try {
-      const result = await this.fetchWithAuth(`/search?${params}`);
+      const result = await this.fetchWithAuth(`/search?q="London Grammar"&type=artist&limit=1`);
       console.log("Search result url:", `/search?${params}`);
       var resultsArtistSearch =  result.artists.items;
       console.log("Search result:", resultsArtistSearch);
